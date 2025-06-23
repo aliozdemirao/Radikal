e46e0d47653df6881aed3cd1f1957e9b554e90a6
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2025-06-23 15:44:14 +0300

    Integrate Hilt for dependency injection
    This commit integrates Hilt for dependency injection into the Android application.
    
    Key changes include:
    
    - Added Hilt dependencies and plugins to `build.gradle.kts` (app and project level) and `libs.versions.toml`.
    - Created a `MainApp` class annotated with `@HiltAndroidApp` and registered it in `AndroidManifest.xml`.
    - Annotated `MainActivity` with `@AndroidEntryPoint`.
    - Moved `MainActivity` to the `ui` package.
    - Removed the `android:label` attribute from the `MainActivity` declaration in `AndroidManifest.xml` as it's typically set by the theme or programmatically.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Hilt
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -56,4 +60,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
--- app/src/main/java/com/aliozdemir/radikal/ui/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/radikal/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aliozdemir.radikal
+package com.aliozdemir.radikal.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aliozdemir.radikal.ui.theme.RadikalTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
